2d24c1f61e7fe6951f59dd99213d6c6954926ef0
Kemelbekov daniyar <kemelbekovdaniyar0@gmail.com>
2023-03-27 09:53:25 +0000

    sort

--- src/components/RegionList/RegionList.js
+++ src/components/RegionList/RegionList.js
@@ -1,4 +1,44 @@
 function RegionList({ regions }) {
+  regions.sort((a, b) => {
+    if (a.name > b.name) {
+      return 1;
+    }
+    else if (a.name < b.name) {
+      return -1;
+    }
+    else {
+      return 0;
+    }
+  });
+
+
+  regions.sort((a, b) => {
+    if (a.area > b.area) {
+      return 1;
+    }
+    else if (a.area < b.area) {
+      return -1;
+    }
+    else {
+      return 0;
+    }
+  });
+
+  regions.sort((a, b) => {
+    if (a.population > b.population) {
+      return 1;
+    }
+    else if (a.population < b.population) {
+      return -1;
+    }
+    else {
+      return 0;
+    }
+  });
+
+
+
+
   const output = regions.map((region) => (
     <div>
       <h2>{region.name}</h2>
